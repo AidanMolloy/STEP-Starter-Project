e66a9d059731a1fc5adeece311dd6f0f13d6662f
AidanMolloy <aidan@molloy.ie>
2020-07-06 12:56:51 +0000

    Fetch name from DataServlet

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -48,3 +48,40 @@ function openContent(evt, page) {
     footerElement.classList.add("minimiseFooter");
   }
 }
+
+/**
+ * Fetches a my name from the server and adds it to the DOM.
+ */
+function getName() {
+  console.log('Fetching my name.');
+
+  // The fetch() function returns a Promise because the request is asynchronous.
+  const responsePromise = fetch('/data');
+
+  // When the request is complete, pass the response into handleResponse().
+  responsePromise.then(handleResponse);
+}
+
+/**
+ * Handles response by converting it to text and passing the result to
+ * addNameToDOM().
+ */
+function handleResponse(response) {
+  console.log('Handling the response.');
+
+  // response.text() returns a Promise, because the response is a stream of
+  // content and not a simple variable.
+  const textPromise = response.text();
+
+  // When the response is converted to text, pass the result into the
+  // addNameToDOM() function.
+  textPromise.then(addNameToDOM);
+}
+
+/** Adds my name to the DOM. */
+function addNameToDOM(name) {
+  console.log('Adding name to dom: ' + name);
+
+  const nameContainer = document.getElementById('name-container');
+  nameContainer.innerText = quote;
+}
