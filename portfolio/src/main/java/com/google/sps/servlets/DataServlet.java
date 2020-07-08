b9c0af1ddb2ffcaca99291dc7ba6425939b61ec9
AidanMolloy <aidan@molloy.ie>
2020-07-08 10:31:56 +0000

    Comments servlet working with 2d Array storing comment data

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -20,47 +20,77 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
-import java.util.*;
+import java.io.*; 
+import java.lang.*; 
+import java.util.*; 
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;  
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
+/** Servlet that returns comment data. */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-  private ArrayList<String> comments = new ArrayList<String>();
+  // Create comments Array
+  public String[][] comments = { {"Aidan Molloy", "Love the website", "12:00 - 01/01/2020"} };
 
   @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Get the input from the form.
+    String userName = getParameter(request, "user-name", "");
+    String comment = getParameter(request, "comment", "");
+
+    // Get current date.
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm - dd/MM/yyyy");
+    LocalDateTime now = LocalDateTime.now();
+
+    // Add the new comment to Array.
+    String[] newComment = {userName, comment, dtf.format(now)};
+    comments = addComment(comments.length, comments, newComment);
+
+    // Redirect user once comment added TODO: Redirect to Comments tab.
+    response.sendRedirect("/index.html#comments");
+  }
+
+  // Function to add a comment to the array
+  public static String[][] addComment(int arrayLength, String arr[][], String commentToBeAdded[])
+  {
+    int i;
+
+    // create a new array of size arrayLength+1
+    String newarr[][] = new String[arrayLength + 1][3];
+
+    // insert the elements from the old array into the new array
+    // insert all elements till arrayLength then insert the newComment at arrayLength+1
+    for (i = 0; i < arrayLength; i++)
+        newarr[i] = arr[i];
+
+    newarr[arrayLength] = commentToBeAdded;
+
+    return newarr;
+  } 
+
+  // Get data from the form
+  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    }
+    return value;
+  }
+
+  // Print the comments data to /data
+  @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    addComments(comments);
     String json = convertToJson(comments);
     response.setContentType("application/json;");
     response.getWriter().println(json);
   }
 
-  private void addComments(ArrayList comments) {
-    comments.add("Aidan M");
-    comments.add("Wow!");
-    comments.add("John D");
-    comments.add("Amazing!");
-    comments.add("Grace F");
-    comments.add("Interesting!");
-  }
-
-  private String convertToJson(ArrayList comments) {
-    String json = "{";
-    int j = 0;
-    for (int i = 0; i < comments.size(); i++) {
-      json += "\"author" + j + "\": ";
-      json += "\"" + comments.get(i) + "\"";
-      json += ", ";
-      i++;
-      json += "\"comment" + j + "\": ";
-      json += "\"" + comments.get(i) + "\"";
-      json += ", ";
-      j++;
-    }
-    json = json.substring(0, json.length()-2);
-    json += "}";
-
+  
+  // Converts comments into a JSON string using the Gson library.
+  private String convertToJson(String[][] comments) {
+    Gson gson = new Gson();
+    String json = gson.toJson(comments);
     return json;
   }
 }
